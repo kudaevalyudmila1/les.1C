24331aa3961e42aab162f9a4eb9b96177c924b37
Lyudmila <kudaevalyudmila@gmail.com>
2022-11-13 20:01:51 +0300

    array index

---
+++ example010/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 86, 2, 30, 4, 58, 6, 97, 86 };
+
+int n = array.Length;
+int find = 86;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
